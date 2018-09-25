4f85c6bec592020d94118bb50c329c0d6b02b4cb
baravak <itb.baravak@gmail.com>
2018-09-25 22:55:06 +0330

    api controller response array bugfix

--- src/Controllers/API/Controller.php
+++ src/Controllers/API/Controller.php
@@ -28,7 +28,8 @@ class Controller {
 	public function response($result, $data = [], $code = 200)
 	{
 		$response = ['is_ok' => true, 'message' => ':)'];
-		$response = is_array($result) ? array_merge($response, $result) : array_merge($response, is_string($result) ? ['message' => $result] : $result);
+		$result = is_object($result) ? $result->toArray() : $result;
+		$response = is_array($result) ? array_merge($response, $result) : array_merge($response, ['message' => $result]);
 		if(is_object($result))
 		{
 			$response = array_merge($response, $result->toArray());
