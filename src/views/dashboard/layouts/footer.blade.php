ec9f51ae048200ffbd4cfbd972e3dcc85af43825
baravak <itb.baravak@gmail.com>
2018-10-21 11:08:04 +0330

    js script src bugfix

--- src/views/dashboard/layouts/footer.blade.php
+++ src/views/dashboard/layouts/footer.blade.php
@@ -2,7 +2,7 @@
 	<script src="/vendors/jquery-3.3.1.min.js"></script>
 	<script src="/vendors/popper.min.js"></script>
 	<script src="/vendors/bootstrap-4.1.3/bootstrap.min.js"></script>
-	<script src="/vendors/iziToast/css/iziToast.min.css"></script>
+	<script src="/vendors/iziToast/js/iziToast.min.js"></script>
 	<script src="/js/dashio.min.js"></script>
 	<script src="/js/main.js"></script>
 @show
