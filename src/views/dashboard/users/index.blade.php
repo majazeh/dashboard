dd5e335125ec7763fc45a72eecbc97e81142171f
baravak <itb.baravak@gmail.com>
2019-01-19 11:50:41 +0330

    users sort icon

--- src/views/dashboard/users/index.blade.php
+++ src/views/dashboard/users/index.blade.php
@@ -1,73 +1,77 @@
 @extends('dashboard.layouts.index')
 
-@section('index.content')
-<div class="table-responsive">
-    <table class="table table-striped table-hover">
-        <thead class="f1">
-            <tr>
-                <th class="text-center">
-                    {{ _d('id') }}
-                    @sort_icon(id)
-                </th>
-                <th>
-                    {{ _d('name') }}
-                    @sort_icon(name)
-                </th>
-                <th>
-                    {{ _d('username') }}
-                    @sort_icon(username)
-                </th>
-                <th>
-                    {{ _d('email') }}
-                </th>
-                <th>
-                    {{ _d('mobile') }}
-                </th>
-                <th>
-                    {{ _d('account.status') }}
-                    @sort_icon(status)
-                </th>
-                <th>
-                    {{ _d('account.type') }}
-                    @sort_icon(type)
-                </th>
-                <th class="text-center">
-                    {{ _d('gender') }}
-                    @sort_icon(gender)
-                </th>
-                <th></th>
-            </tr>
-        </thead>
-        <tbody class="f2">
-            @foreach ($users as $user)
-            <tr>
-                <td class="text-center">
-                    {{ $user->id }}
-                </td>
-                <td>
-                    <a class="text-dark" href="{{ route($module->resource . '.edit', $user->id) }}">
-                        {{ $user->name }}
-                    </a>
-                </td>
-                <td>{{ $user->username }}</td>
-                <td>{{ $user->email }}</td>
-                <td>{{ $user->mobile }}</td>
-                <td class="{{ $user_status_css[$user->status] }}">
-                    {{ _d('status.' . $user->status) }}
-                </td>
-                <td>{{ _d('type.' . $user->type) }}</td>
-                <td class="text-center">
-                    <i class="fas fa-{{ $user->gender ?: 'genderless' }} {{ $user->gender == 'male' ? 'text-primary' : ($user->gender == 'female' ? 'text-info' : '')}}"></i>
-                </td>
-                <td class="d-flex justify-content-around">
-                    <a class="text-secondary" href="{{ route($module->resource . '.edit', $user->id) }}">
-                        <i class="fas fa-edit"></i>
-                    </a>
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-    {{ $users->links() }}
+@section('container-fluid')
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-striped table-hover">
+                        <thead class="f2">
+                            <tr>
+                                <th class="text-center">
+                                    {{ _d('id') }}
+                                    @sort_icon(id)
+                                </th>
+                                <th>
+                                    {{ _d('name') }}
+                                    @sort_icon(name)
+                                </th>
+                                <th>
+                                    {{ _d('username') }}
+                                    @sort_icon(username)
+                                </th>
+                                <th>
+                                    {{ _d('email') }}
+                                </th>
+                                <th>
+                                    {{ _d('mobile') }}
+                                </th>
+                                <th>
+                                    {{ _d('account.status') }}
+                                    @sort_icon(status)
+                                </th>
+                                <th>
+                                    {{ _d('account.type') }}
+                                    @sort_icon(type)
+                                </th>
+                                <th class="text-center">
+                                    {{ _d('gender') }}
+                                    @sort_icon(gender)
+                                </th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody class="f1">
+                            @foreach ($users as $user)
+                            <tr>
+                                <td class="text-center">{{ $user->id }}</td>
+                                <td>
+                                    <a class="text-dark" href="{{ route($module->resource . '.edit', $user->id) }}">{{ $user->name }}</a>
+                                </td>
+                                <td>{{ $user->username }}</td>
+                                <td>{{ $user->email }}</td>
+                                <td>{{ $user->mobile }}</td>
+                                <td class="{{ $user_status_css[$user->status] }}">
+                                    {{ _d('status.' . $user->status) }}
+                                </td>
+                                <td>{{ _d('type.' . $user->type) }}</td>
+                                <td class="text-center">
+                                    <i class="fas fa-{{ $user->gender ?: 'genderless' }} {{ $user->gender == 'male' ? 'text-primary' : ($user->gender == 'female' ? 'text-info' : '')}}"></i>
+                                </td>
+                                <td class="text-center">
+                                    <a class="text-secondary" href="{{ route($module->resource . '.edit', $user->id) }}">
+                                        <i class="fas fa-edit"></i>
+                                    </a>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                    {{ $users->links() }}
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
