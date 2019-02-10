0edfa3c6c3b4c44eb5185e9d4ca6015576e3b860
baravak <itb.baravak@gmail.com>
2019-02-10 13:30:28 +0330

    navigate bugfix

--- src/views/dashboard/layouts/navigation.blade.php
+++ src/views/dashboard/layouts/navigation.blade.php
@@ -28,12 +28,12 @@
                 <i class="fal fa-bell p-2 text-muted"></i>
             </a>
             <a tabindex='0'
-             class='navigation-profile-image'
+             class='navigation-profile-image action'
              role='button'
-             data-content='<div><a class="btn btn-sm btn-danger btn-gradient" href="{{ route('logout') }}" class="text-danger f2">خروج</a></div>'
+             data-content='<div><form action="{{ route('logout') }}" method="post">@csrf<button class="btn btn-sm btn-danger btn-gradient action" data-method="post" href="{{ route('logout') }}" class="text-danger f2">خروج</button></form></div>'
              data-toggle='popover'
              data-html='true'
-             data-placement='bottom'>
+             data-placement='bottom' href="javascript:return false">
                 <img src="{{ asset('images/profile-image.jpg') }}" alt="{{ \Auth::user()->name}}" width="40" height="40">
             </a>
             @endif
