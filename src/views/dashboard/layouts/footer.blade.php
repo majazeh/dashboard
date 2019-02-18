9bf93a0795222585f17c790b373c8976c8fc877f
baravak <itb.baravak@gmail.com>
2019-02-18 16:21:11 +0330

    bugfix cache static link

--- src/views/dashboard/layouts/footer.blade.php
+++ src/views/dashboard/layouts/footer.blade.php
@@ -4,6 +4,6 @@
 	<script src="{{ asset('vendors/bootstrap-4.2.1/js/bootstrap.min.js') }}"></script>
 	<script src="{{ asset('vendors/iziToast/js/iziToast.min.js') }}"></script>
     <script src="{{ asset('vendors/jquery-touchswipe/jquery.touchSwipe.min.js') }}"></script>
-	<script src="{{ asset('js/dashio.min.js') }}"></script>
-	<script src="{{ asset('js/main.js') }}"></script>
+	<script src="{{ asset('js/dashio.min.js') . '?v=' . filemtime(public_path('js/main.js'))  }}"></script>
+	<script src="{{ asset('js/main.js') . '?v=' . filemtime(public_path('js/main.js'))  }}"></script>
 @show
