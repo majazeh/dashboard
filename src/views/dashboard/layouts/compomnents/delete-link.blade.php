00a875f7e99edecf8c533f0063edc94f14dc4686
moab7698 <moab7698@gmail.com>
2019-02-21 13:30:49 +0330

    hasan edit

--- src/views/dashboard/layouts/compomnents/delete-link.blade.php
+++ src/views/dashboard/layouts/compomnents/delete-link.blade.php
@@ -1,4 +1,3 @@
-
-<a class="text-secondary" href="{{ $link }}" class="action">
+<a class="text-secondary" href="{{ $link }}" data-lijax="click" data-method="DELETE">
 	<i class="far fa-trash-alt"></i>
 </a>
