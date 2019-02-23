5746685646eacd134598541cf6b8a1378166ab9f
Ahmad <ahmadkarimi1991@gmail.com>
2019-02-23 16:13:45 +0330

    add select2 to header and footer layout files

--- src/views/dashboard/layouts/footer.blade.php
+++ src/views/dashboard/layouts/footer.blade.php
@@ -4,6 +4,7 @@
 	<script src="{{ asset('vendors/bootstrap-4.2.1/js/bootstrap.min.js') }}"></script>
 	<script src="{{ asset('vendors/iziToast/js/iziToast.min.js') }}"></script>
     <script src="{{ asset('vendors/jquery-touchswipe/jquery.touchSwipe.min.js') }}"></script>
+    <script src="{{ asset('vendors/select2/select2.min.js') }}"></script>
 	<script src="{{ asset('js/dashio.min.js') . '?v=' . filemtime(public_path('js/dashio.min.js'))  }}"></script>
 	<script src="{{ asset('js/main.js') . '?v=' . filemtime(public_path('js/main.js'))  }}"></script>
 @show
