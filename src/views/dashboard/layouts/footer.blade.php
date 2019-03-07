378a3cec8256972d669e66b61648e8622d9c471c
Ahmad <ahmadkarimi1991@gmail.com>
2019-03-07 13:06:08 +0330

    datepicker static files address bugfix

--- src/views/dashboard/layouts/footer.blade.php
+++ src/views/dashboard/layouts/footer.blade.php
@@ -6,7 +6,7 @@
     <script src="{{ asset('vendors/jquery-touchswipe/jquery.touchSwipe.min.js') }}"></script>
     <script src="{{ asset('vendors/select2/select2.min.js') }}"></script>
     <script src="{{ asset('vendors/moment.min.js') }}"></script>
-    <script src="{{ asset('vendors/Date-Time-Picker-Bootstrap-4/bootstrap-datetimepicker.min.js') }}"></script>
+    <script src="{{ asset('vendors/Date-Time-Picker-Bootstrap-4/js/bootstrap-datetimepicker.min.js') }}"></script>
 	<script src="{{ asset('js/dashio.min.js') . '?v=' . filemtime(public_path('js/dashio.min.js'))  }}"></script>
 	<script src="{{ asset('js/main.js') . '?v=' . filemtime(public_path('js/main.js'))  }}"></script>
 @show
