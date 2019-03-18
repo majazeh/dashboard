79639be599cc65444954b2a341f18447aea703ff
baravak <itb.baravak@gmail.com>
2019-03-18 16:32:24 +0330

    paginate add default parameters

--- src/Controllers/Paginate.php
+++ src/Controllers/Paginate.php
@@ -4,18 +4,18 @@ use Illuminate\Http\Request;
 
 trait Paginate
 {
-	public function paginate_order(Request $request, $model, $order_list = ['id'])
+	public function paginate_order(Request $request, $model, $order_list = ['id'], $default = ['id', 'desc'])
 	{
 		$keys = array_keys($order_list);
-		$order = $request->input('order') && in_array($request->input('order'), $keys) ? strtolower($request->input('order')) : 'id';
+		$order = $request->input('order') && in_array($request->input('order'), $keys) ? strtolower($request->input('order')) : $default[0];
 		if(isset($order_list[$order]))
 		{
 			$order = $order_list[$order];
 		}
-		$sort = strtolower($request->input('sort')) == 'asc' ? 'asc' : 'desc';
+		$sort = strtolower($request->input('sort')) == 'asc' ? 'asc' : $default[1];
 		$model->orderBy($order, $sort);
 		$paginate = $model->paginate();
-		if($order != 'id' || $sort != 'desc')
+		if($order != $default[0] || $sort != $default[1])
 		{
 	        $paginate->appends($request->all('order', 'sort'));
 		}
