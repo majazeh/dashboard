906707baa38d88637b3361104ffe616a2b3e57a8
Ahmad <ahmadkarimi1991@gmail.com>
2019-04-14 18:44:33 +0430

    add meta

--- src/views/dashboard/layouts/header.blade.php
+++ src/views/dashboard/layouts/header.blade.php
@@ -10,9 +10,33 @@
 	<link rel="stylesheet" type="text/css" href="{{ asset('vendors/Date-Time-Picker-Bootstrap-4/css/bootstrap-datetimepicker.min.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/dashio.min.css') . '?v=' . filemtime(public_path('css/dashio.min.css'))  }}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('css/main.css') . '?v=' . filemtime(public_path('css/main.css'))  }}">
-	@if (file_exists(public_path('images/favicon.ico')))
-	<link rel="icon" href="{{asset('images/favicon.ico')}}?v={{filemtime(public_path('images/favicon.ico'))}}" type="image/x-icon" />
+
+	@if (file_exists(public_path('favicon.ico')))
+	<link rel="icon" href="{{ asset('favicon.ico') }}?v={{ filemtime(public_path('favicon.ico')) }}" type="image/x-icon" />
 	@endif
+
+	@if (file_exists(public_path('apple-touch-icon.png')))
+		<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}?v={{ filemtime(public_path('apple-touch-icon.png')) }}">
+	@endif
+
+	@if (file_exists(public_path('favicon-32x32.png')))
+		<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}?v={{ filemtime(public_path('favicon-32x32.png')) }}">
+	@endif
+
+	@if (file_exists(public_path('favicon-16x16.png')))
+		<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}?v={{ filemtime(public_path('favicon-16x16.png')) }}">
+	@endif
+
+	@if (file_exists(public_path('site.webmanifest')))
+	<link rel="manifest" href="/site.webmanifest">
+	@endif
+
+	@if (file_exists(public_path('safari-pinned-tab.svg')))
+		<link rel="mask-icon" href="{{ asset('safari-pinned-tab.svg') }}?v={{ filemtime(public_path('safari-pinned-tab.svg')) }}" color="#ffde00">
+	@endif
+
+	<meta name="msapplication-TileColor" content="#ffde00">
+	<meta name="theme-color" content="#ffde00">
 @show
 
 <title>{{ $global->title ?: _d('Dashio') }}</title>
