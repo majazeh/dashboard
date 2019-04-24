eff41dbe5d89664bc2a901a81959a32bfb558e04
baravak <itb.baravak@gmail.com>
2019-04-24 12:18:34 +0430

    add custom class to body tag

--- src/views/dashboard/layouts/app.blade.php
+++ src/views/dashboard/layouts/app.blade.php
@@ -6,7 +6,7 @@
 </head>
 
 @section('body-tag')
-    <body class="d-flex rtl">
+    <body class="d-flex rtl{{isset($module->bodyClass) ? ' '. join(' ', $module->bodyClass) : ''}}">
         @section('body')
             @include('dashboard.layouts.body')
         @show
