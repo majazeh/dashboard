0d457c95a3cf5090fc3a62eafb7c21ef639c79ab
baravak <itb.baravak@gmail.com>
2019-05-06 15:35:07 +0430

    user breadcrumbs set

--- src/routes/breadcrumbs.php
+++ src/routes/breadcrumbs.php
@@ -3,4 +3,17 @@ Breadcrumbs::for('dashboard', function ($trail, $data) {
 
     $trail->push(_d('dashboard'), route('dashboard'));
 });
+
+Breadcrumbs::for('users.index', function ($trail, $data) {
+    $trail->parent('dashboard', $data);
+    $trail->push(_d('users.index') . (isset($data['users']) ? ' (' . $data['users']->total() . ')' : ''), route('users.index'));
+});
+Breadcrumbs::for('users.create', function ($trail, $data) {
+    $trail->parent('users.index', $data);
+    $trail->push(_d('users.create') , route('users.create'));
+});
+Breadcrumbs::for('users.edit', function ($trail, $data) {
+    $trail->parent('users.index', $data);
+    $trail->push(_d('users.edit') , route('users.edit', $data['user']->id));
+});
 ?>
