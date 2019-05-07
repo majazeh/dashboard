0f9fc99aec4b5b89b4c9bbf593bce080a39431b0
baravak <itb.baravak@gmail.com>
2019-05-07 16:08:46 +0430

    upgrade paginate to get array

--- src/Controllers/Paginate.php
+++ src/Controllers/Paginate.php
@@ -7,15 +7,21 @@ trait Paginate
 	public function paginate_order(Request $request, $model, $order_list = ['id'], $default = ['id', 'desc'])
 	{
 		$keys = array_keys($order_list);
-		$order = $request->input('order') && in_array($request->input('order'), $keys) ? strtolower($request->input('order')) : $default[0];
-		if(isset($order_list[$order]))
+		$order_string = $request->input('order') && in_array($request->input('order'), $keys) ? strtolower($request->input('order')) : $default[0];
+		$orders = explode(',', $order_string);
+		
+		$sort_string = $request->sort ?: $default[1];
+		$sorts = explode(',', $sort_string);
+		foreach($orders as $key => $order)
 		{
-			$order = $order_list[$order];
+			if(isset($order_list[$order]))
+			{
+				$sort = isset($sorts[$key]) && in_array(strtolower($sorts[$key]), ['asc', 'desc']) ? strtolower($sorts[$key]) : 'desc';
+				$model->orderBy($order, $sort);
+			}
 		}
-		$sort = strtolower($request->input('sort')) == 'asc' ? 'asc' : $default[1];
-		$model->orderBy($order, $sort);
 		$paginate = $model->paginate();
-		if($order != $default[0] || $sort != $default[1])
+		if($order_string != $default[0] || $sort_string != $default[1])
 		{
 	        $paginate->appends($request->all('order', 'sort'));
 		}
