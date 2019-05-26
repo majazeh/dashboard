5180e7a9ee7e2033762987ed31e3301873983d3e
Ahmad <ahmadkarimi1991@gmail.com>
2019-05-26 16:01:30 +0430

    minor improvements

--- src/Controllers/Requests.php
+++ src/Controllers/Requests.php
@@ -7,6 +7,7 @@ trait Requests
 	public function index(Request $request)
 	{
 		$this->access_check('index', ...func_get_args());
+		$args = func_get_args();
 		$parent = isset($this->parent) && isset(func_get_args()[1]) ? func_get_args()[1] : null;
 		if($parent)
 		{
@@ -20,7 +21,7 @@ trait Requests
 			$this->fast_search($request, $model);
 		}
 		\Data::set($list_name, $this->paginate_order($request, $model, isset($this->ordering) ? $this->ordering : ['id']));
-		return $this->view("{$this->resource}.index");
+		return $this->index_view($request, $parent, array_splice($args, 2));
 	}
 
 	public function index_query($request, $parent = null)
@@ -28,9 +29,15 @@ trait Requests
 		return $this->get_model()::select('*');
 	}
 
+	public function index_view(Request $request)
+	{
+		return $this->view("{$this->resource}.index");
+	}
+
 	public function show(Request $request, $id)
 	{
 		$this->access_check('show', ...func_get_args());
+		$args = func_get_args();
 		$parent = null;
 		if(isset($this->parent) && isset(func_get_args()[2]))
 		{
@@ -41,10 +48,10 @@ trait Requests
 		{
 			\Data::set(strtolower($this->parent), $this->getParent()::findOrFail($parent));
 		}
-		$table = $this->show_query($request, $id, $parent);
+		$table = $this->show_query($request, $id, $parent, ...array_splice($args, 3));
 		\Data::set(strtolower($this->table), $table);
 		\Data::set("id", $table->id);
-		return $this->view("{$this->resource}.show");
+		return $this->show_view($request, $id, $parent, ...array_splice($args, 3));
 	}
 
 	public function show_query($request, $id, $parent = null)
@@ -52,20 +59,32 @@ trait Requests
 		return $this->findOrFail($id);
 	}
 
+	public function show_view(Request $request)
+	{
+		return $this->view("{$this->resource}.show");
+	}
+
 	public function create(Request $request)
 	{
 		$this->access_check('create', ...func_get_args());
+		$args = func_get_args();
 		$parent = isset($this->parent) && isset(func_get_args()[1]) ? func_get_args()[1] : null;
 		if($parent)
 		{
 			\Data::set(strtolower($this->parent), $this->getParent()::findOrFail($parent));
 		}
+		return $this->create_view($request, $parent, ...array_splice($args, 2));
+	}
+
+	public function create_view(Request $request)
+	{
 		return $this->view("{$this->resource}.create");
 	}
 
 	public function edit(Request $request, $id)
 	{
 		$this->access_check('edit', ...func_get_args());
+		$args = func_get_args();
 		$parent = null;
 		if(isset($this->parent) && isset(func_get_args()[2]))
 		{
@@ -76,10 +95,15 @@ trait Requests
 		{
 			\Data::set(strtolower($this->parent), $this->getParent()::findOrFail($parent));
 		}
-		$table = $this->show_query($request, $id, $parent);
+		$table = $this->show_query($request, $id, $parent, ...array_splice($args, 3));
 		\Data::set(strtolower($this->table), $table);
 		\Data::set("id", $table->id);
 		\Data::set("action", 'edit');
+		return $this->edit_view($request, $id, $parent, ...array_splice($args, 3));
+	}
+
+	public function edit_view(Request $request)
+	{
 		return $this->view("{$this->resource}.create");
 	}
 
@@ -120,10 +144,10 @@ trait Requests
 		$parent = isset($this->parent) && isset(func_get_args()[1]) ? func_get_args()[1] : null;
 		if($parent)
 		{
-			$this->getParent()::findOrFail($parent);
+			$parent = $this->getParent()::findOrFail($parent);
 		}
-		$this->validator($request)->validate();
-		$this->validated($request);
+		$this->validator($request, false, $parent)->validate();
+        $this->validated($request, false, $parent);
 		$data = [];
 		foreach ($this->get_fields('keys', $request) as $key => $value) {
 			if($request->input($value))
@@ -131,9 +155,13 @@ trait Requests
 				$data[$value] = $request->input($value);
 			}
 		}
-		$create = $this->get_model()::create($data);
+		$create = $this->store_transaction($request, $data, $parent);
 		$response = $this->response_store($request, $create, $parent);
 		return $this->response($response);
+	}
+
+	public function store_transaction($request, $data, &$parent = null){
+		$this->get_model()::create($data);
 	}
 	public function response_store($request, $create, $parent)
 	{
@@ -167,16 +195,19 @@ trait Requests
 		{
 			$this->getParent()::findOrFail($parent);
 		}
-		$table = $this->show_query($request, $id, $parent);
-		$this->validator($request, $table)->validate();
-		$this->validated($request, $table);
-		$original_all = $table->toArray();
+		$row = $this->show_query($request, $id, $parent);
+		$this->validator($request, $row, $parent)->validate();
+        $this->validated($request, $row, $parent);
+		$original_all = $row->toArray();
 		foreach ($this->get_fields('inputs', $request, true) as $key => $value) {
-			$table->$key = $value;
+			$row->$key = $value;
 		}
-		$table->save();
-		$response = $this->response_update($request, $table, $parent, $original_all);
+		$this->update_transaction($request, $row, $parent);
+		$response = $this->response_update($request, $row, $parent, $original_all);
 		return $this->response($response);
+	}
+	public function update_transaction($request, &$row, &$parent = null){
+		$row->save();
 	}
 	public function response_update($request, $table, $parent, $original_all)
 	{
