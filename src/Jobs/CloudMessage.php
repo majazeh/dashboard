57bdda3e4ab6118e24211459fcc6111506746fb3
baravak <itb.baravak@gmail.com>
2019-06-06 16:43:12 +0430

    firebase token autoremove

--- src/Jobs/CloudMessage.php
+++ src/Jobs/CloudMessage.php
@@ -8,7 +8,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Mail;
-
+use \App\FirebaseToken;
 class CloudMessage implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
@@ -33,12 +33,25 @@ class CloudMessage implements ShouldQueue
      */
     public function handle()
     {
-		$client = new \GuzzleHttp\Client(['defaults' => [ 'exceptions' => false ]]);
-		$response = $client->post($this->url, [
-			\GuzzleHttp\RequestOptions::JSON => $this->details,
-			\GuzzleHttp\RequestOptions::HEADERS => [
-				'Authorization' => 'KEY='.config('firebase.lck')
-			]
-		]);
+        $client = new \GuzzleHttp\Client(['defaults' => [ 'exceptions' => false ]]);
+        $response = $client->post($this->url, [
+            \GuzzleHttp\RequestOptions::JSON => $this->details,
+            \GuzzleHttp\RequestOptions::HEADERS => [
+                'Authorization' => 'KEY='.config('firebase.lck')
+            ]
+        ]);
+        $body = json_decode($response->getBody()->getContents());
+        if($body && isset($body->results) && is_array($body->results))
+        {
+            $delete_tokens = [];
+            foreach ($body->results as $key => $value) {
+                if(isset($value->error) && $value->error == 'NotRegistered')
+                {
+                    $ids = isset($this->details['to']) ? $this->details['to'] : $this->details['registration_ids'];
+                    $delete_tokens[] = $ids[$key];
+                }
+            }
+            FirebaseToken::whereIn('token', $delete_tokens)->delete();
+        }
     }
 }
