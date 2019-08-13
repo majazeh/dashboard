4ce828467f211bda5ee0a0d648215f6b932ce69c
baravak <itb.baravak@gmail.com>
2019-08-13 13:15:11 +0430

    add serial checker

--- src/Models/Traits/Serial.php
+++ src/Models/Traits/Serial.php
@@ -20,4 +20,11 @@ trait Serial
 		}
 		return \App\Serial::decode(substr($serial, strlen(self::$s_prefix))) - self::$s_start;
 	}
+
+	public static function serialCheck($serial)
+	{
+		$id = self::id($serial);
+		if(!$id || ($id + self::$s_start) < self::$s_start || self::$s_end < ($id + self::$s_start)) return false;
+		return true;
+	}
 }
